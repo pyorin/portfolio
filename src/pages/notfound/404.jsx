00b23777f0122a0bf5264bab149b1d59ce2d467e
angga <maulani.morningstar@gmail.com>
2023-01-23 11:27:54 +0700

    update

--- src/pages/notfound/404.jsx
+++ src/pages/notfound/404.jsx
@@ -1,8 +1,23 @@
 import Layout from "../../components/layout/Layout";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Notfound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  }, [navigate]);
+
   const content = (
-    <h1 className="text-2xl font-semibold text-center">404 not found</h1>
+    <>
+      <h1 className="text-2xl font-semibold text-center">404 not found</h1>
+      <h1 className="text-2xl font-semibold text-center">
+        you will be redirected to homepage...
+      </h1>
+    </>
   );
 
   return <Layout content={content} />;
